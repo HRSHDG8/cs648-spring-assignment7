6d4f14e4e2f52a5cca72cc070dc1ed0c8085b2d5
HRSHDG8 <hrshdg8@gmail.com>
2022-03-23 18:57:57 -0400

    Fix api

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -5,15 +5,35 @@
  *   mongo myFirstDatabase scripts/init.mongo.js
  * Atlas:
  *   mongosh "mongodb+srv://hmac:cs648@cluster0.uimtv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
- * or open mongo shell navigate to current directory
- *  and run load('scripts/init.mongo.js')
+ *   and run load('scripts/init.mongo.js')
  */
 
 /* global db print */
 /* eslint no-restricted-globals: "off" */
 
 db.products.deleteMany({});
+db.deleted_products.remove({});
+
+const initialProducts = [
+  {
+    id: 1,
+    name: "Levi's",
+    category: 'Jeans',
+    price: '30',
+    imageUrl: 'https://picsum.photos/500/500',
+  },
+  {
+    id: 2,
+    name: 'Allen Solly',
+    category: 'Shirts',
+    price: '25',
+    imageUrl: 'https://picsum.photos/500/500',
+  },
+];
+
+db.products.insertMany(initialProducts);
 const count = db.products.countDocuments();
+
 print('Inserted total of ', count, 'products');
 
 db.counters.deleteOne({ _id: 'products' });
@@ -23,3 +43,4 @@ db.products.createIndex({ id: 1 }, { unique: true });
 db.products.createIndex({ name: 1 });
 db.products.createIndex({ price: 1 });
 db.products.createIndex({ category: 1 });
+db.deleted_products.createIndex({ id: 1 }, { unique: true });
--- api/server.js
+++ api/server.js
@@ -1,74 +1,16 @@
-const fs = require('fs');
 require('dotenv').config();
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { MongoClient } = require('mongodb');
-
-const COUNTERS = 'counters';
-const PRODUCTS = 'products';
-
-const url = process.env.DB_URL
-  || 'mongodb+srv://hmac:cs648@cluster0.uimtv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-let db;
-
-const uidForDocument = async (name) => {
-  const result = await db
-    .collection(COUNTERS)
-    .findOneAndUpdate(
-      { _id: name },
-      {
-        $inc: { uid: 1 },
-        $set: { _id: name },
-      },
-      { returnOriginal: false, upsert: true },
-
-    );
-  return result.value.uid;
-};
-
-const productList = async () => db.collection(PRODUCTS).find({}).toArray();
-
-const addProduct = async (_, { product }) => {
-  const productInsert = { ...product };
-  productInsert.id = await uidForDocument(PRODUCTS);
-  const result = await db.collection(PRODUCTS).insertOne(productInsert);
-  return db
-    .collection(PRODUCTS)
-    .findOne({ _id: result.insertedId });
-};
-
-const connectToDb = async () => {
-  const client = new MongoClient(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  await client.connect();
-  console.log('Connected to MongoDB at', url);
-  db = client.db();
-};
-
-const resolvers = {
-  Query: {
-    productList,
-  },
-  Mutation: {
-    addProduct,
-  },
-};
-
-const server = new ApolloServer({
-  typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
-  resolvers,
-});
+const { connectToDb } = require('./db.js');
+const { bootApp } = require('./api_handler.js');
 
 const app = express();
 
-server.applyMiddleware({ app, path: '/graphql' });
+bootApp(app);
 
 const port = process.env.API_SERVER_PORT || 3000;
 
-const run = async () => {
+// eslint-disable-next-line func-names
+(async function () {
   try {
     await connectToDb();
     app.listen(3000, () => {
@@ -77,6 +19,4 @@ const run = async () => {
   } catch (error) {
     console.log('Error connecting to DB - ', error);
   }
-};
-
-run();
+}());
